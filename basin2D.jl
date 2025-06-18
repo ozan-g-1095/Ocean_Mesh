8db5adbcc08003a33b6092aa5392656ddda3560b
Henry Peterson <hgpeterson@caltech.edu>
2025-06-18 10:41:46 -0700

    Experiment with extrusion

--- basin2D.jl
+++ basin2D.jl
@@ -27,8 +27,12 @@ gmsh.model.geo.addLine(nx, 1)
 gmsh.model.geo.addCurveLoop(1:nx)
 gmsh.model.geo.addPlaneSurface([1], 1)
 
+# # extrude the surface to create a volume (for fun)
+# gmsh.model.geo.extrude([(2, 1)], 0, 1, 0)
+
 # generate
 gmsh.model.geo.synchronize()
 gmsh.model.mesh.generate(2)
+# gmsh.model.mesh.generate(3) # if you want to do the extrusion
 gmsh.write("basin2D.msh")
 gmsh.finalize()

17fe193a7bcbd18a98b04061af0fc86d8ac96e07
Henry Peterson <hgpeterson@caltech.edu>
2025-08-08 10:02:19 -0700

    Fill Panama

--- topo_data/filter_topo.jl
+++ topo_data/filter_topo.jl
@@ -17,12 +17,13 @@ Land points (``z > 0``) are ignored.
 
 See also [`circle_average`](@ref).
 """
-function filter_topo(lon, lat, z; L=1e5)
+function filter_topo(lon, lat, z; L=1e5, ocean_only=false)
     z_filtered = copy(z)
     @showprogress for i in axes(z, 1), j in axes(z, 2)
-        # if z[i, j] <= 0 # only filter ocean points
-            z_filtered[i, j] = circle_average(lon, lat, z, i, j; D=L)
-        # end
+        if z[i, j] > 0 && ocean_only
+            continue # skip land points
+        end
+        z_filtered[i, j] = circle_average(lon, lat, z, i, j; D=L)
     end
     return z_filtered
 end
@@ -115,36 +116,75 @@ function plot_H(lon, lat, z, fname; vmax=6000)
             xlabel="Longitude (°)", ylabel="Latitude (°)",
             cb_title=L"$H$ (m)", aspect_ratio=:equal,
             clims=(0, vmax), 
-            c=cgrad(:magma, rev=true),
-            # c=:seaborn_rocket_gradient,
+            # c=cgrad(:magma, rev=true),
+            c=:seaborn_rocket_gradient,
             grid=false, xlimits=(lon[1], lon[end]),
             ylimits=(lat[1], lat[end]), tickdirection=:out)
     savefig(fname)
     @info "Saved '$fname'"
 end
+function plot_H_panama(lon, lat, z, fname; vmax=6000)
+    lon_min, lon_max = -95.0, -65.0
+    lat_min, lat_max = 3.0, 18.0
+    lon_p = lon[lon .>= lon_min .&& lon .<= lon_max]
+    lat_p = lat[lat .>= lat_min .&& lat .<= lat_max]
+    z_p = z[lon .>= lon_min .&& lon .<= lon_max, lat .>= lat_min .&& lat .<= lat_max]
+    plot_H(lon_p, lat_p, z_p, fname; vmax=vmax)
+end
 
-# settings
-# name = "blacksea"; res = "5min"; L = 50 # filter scale in km
-name = "global"; res = "60min"; L = 500 # filter scale in km
+function fill_panama!(lon, lat, z)
+    lon_mask = lon .>= -80 .&& lon .<= -75
+    lat_mask = lat .>= 8 .&& lat .<= 9
+    z[lon_mask, lat_mask] .= NaN
+    lon_mask = lon .>= -82 .&& lon .<= -76
+    lat_mask = lat .>= 9 .&& lat .<= 10
+    z[lon_mask, lat_mask] .= NaN
+    lon_mask = lon .>= -83 .&& lon .<= -82
+    lat_mask = lat .>= 10 .&& lat .<= 11
+    z[lon_mask, lat_mask] .= NaN
+    lon_mask = lon .>= -84 .&& lon .<= -83
+    lat_mask = lat .>= 10 .&& lat .<= 13
+    z[lon_mask, lat_mask] .= NaN
+    return z
+end
 
 # load data
-d = jldopen(joinpath(@__DIR__, "etopo_$(name)_$(res).jld2"))
+# name = "blacksea"; res = "5min"
+name = "global"; res = "60min"
+ifile = joinpath(@__DIR__, "etopo_$(name)_$(res).jld2")
+@info "Loading data from '$ifile'..."
+d = jldopen(ifile)
 lon = d["lon"]
 lat = d["lat"]
 z = d["z"]
 close(d)
 @info "Dimensons" length(lon) length(lat) size(z)
+# plot_H_panama(lon, lat, z, joinpath(@__DIR__, "H_$(name)_panama.png"))
 
 # filter
-z = filter_topo(lon, lat, z; L=L*1e3) # convert km to m
-ofile = joinpath(@__DIR__, "etopo_$(name)_$(res)_$(L)km.jld2")
+L_land = 250e3 # 250 km
+L_ocean = 500e3 # 500 km
+z = filter_topo(lon, lat, z; L=L_land,  ocean_only=false) 
+z = filter_topo(lon, lat, z; L=L_ocean, ocean_only=true) 
+ofile = joinpath(@__DIR__, @sprintf("etopo_%s_%s_%dkm_%dkm.jld2", name, res, L_land/1e3, L_ocean/1e3))
 jldsave(ofile; lon, lat, z)
 @info "Saved '$ofile'"
 
 # plot
-d = jldopen(joinpath(@__DIR__, "etopo_$(name)_$(res)_$(L)km.jld2"))
+ifile = joinpath(@__DIR__, @sprintf("etopo_%s_%s_%dkm_%dkm.jld2", name, res, L_land/1e3, L_ocean/1e3))
+@info "Loading filtered data from '$ifile'..."
+d = jldopen(ifile)
 lon = d["lon"]
 lat = d["lat"]
 z = d["z"]
 close(d)
 plot_H(lon, lat, z, joinpath(@__DIR__, "H_$(name)_filtered.png"))
+
+# fill Panama region
+plot_H_panama(lon, lat, z, joinpath(@__DIR__, "H_$(name)_filtered_panama.png"))
+z = fill_panama!(lon, lat, z)
+plot_H(lon, lat, z, joinpath(@__DIR__, "H_$(name)_filtered_panama_filled.png"))
+plot_H_panama(lon, lat, z, joinpath(@__DIR__, "H_$(name)_filtered_panama_filled_panama.png"))
+ofile = joinpath(@__DIR__, @sprintf("etopo_%s_%s_%dkm_%dkm_panama_filled.jld2", name, res, L_land/1e3, L_ocean/1e3))
+jldsave(ofile; lon, lat, z)
+@info "Saved '$ofile'"
